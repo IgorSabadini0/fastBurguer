2f5e10e705dc113de1caa2b00ca94208cb6fb76d
igorsabadini0 <igorsabadini157@gmail.com>
2026-09-15 20:34:27 -0300

    feat: login page

--- App.tsx
+++ App.tsx
@@ -1,12 +1,35 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import LoginScreen from './src/screens/LoginScreen';
+import React, { useState, useEffect } from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+export type RootStackParamList = {
+  Login: undefined;
+};
+
+const Stack = createNativeStackNavigator<RootStackParamList>();
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Fast Burguer</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="Login">
+          {(props) => (
+            <LoginScreen
+              onLogin={() =>
+                props.navigation.reset({
+                  index: 0,
+                  routes: [{ name: 'Main' }],
+                })
+              }
+            />
+          )}
+        </Stack.Screen>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
